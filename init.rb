a43da9493be86b374070154ae3983a7ad6d2a3b1
Andres Montano <amontano@virginia.edu>
2010-02-08 14:09:46 -0600

    Using i18n syntax to load locales since globalize2 doesn't seem to override << method.

--- init.rb
+++ init.rb
@@ -1,5 +1,6 @@
 # Include hook code here
 require 'csv'
 
-I18n.load_path << File.join(File.dirname(__FILE__), 'config', 'locales')
+# I18n.load_path << File.join(File.dirname(__FILE__), 'config', 'locales')
+I18n.load_path += Dir[File.join(File.dirname(__FILE__), 'config', 'locales', '**', '*.yml')]
 require File.join(File.dirname(__FILE__), 'app', 'sweepers', 'category_sweeper')
