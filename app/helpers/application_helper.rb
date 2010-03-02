d32fe50654fb929e1582978044a00455bba6e028
Heiser Mazariegos <hm5u@virginia.edu>
2010-03-02 15:23:40 -0600

    made formated_mms_pages accessible to all classes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -29,4 +29,21 @@ module ApplicationHelper
     when 3 then [list[0..size-2].join(', '), list[size-1]].join(', and ')
     end
   end
+  
+  def formated_mms_pages(s) 
+    str = "#{s.start_page}"
+    if !s.start_line.nil?
+      str += '.' + "#{s.start_line}"
+    end
+    if !s.end_page.nil? or !s.end_line.nil?
+      str += '-'
+      if !s.end_page.nil?
+        str += "#{s.end_page}"
+      end
+      if !s.end_line.nil?
+        str += '.' + "#{s.end_line}."
+      end
+    end
+    return str
+  end
 end
--- app/helpers/sources_helper.rb
+++ app/helpers/sources_helper.rb
@@ -6,21 +6,4 @@ module SourcesHelper
   def javascripts
     [super, include_tiny_mce_if_needed].join("\n")
   end  
-  
-  def formated_mms_pages(s) 
-    str = "#{s.start_page}"
-    if !s.start_line.nil?
-      str += '.' + "#{s.start_line}"
-    end
-    if !s.end_page.nil? or !s.end_line.nil?
-      str += '-'
-      if !s.end_page.nil?
-        str += "#{s.end_page}"
-      end
-      if !s.end_line.nil?
-        str += '.' + "#{s.end_line}."
-      end
-    end
-    return str
-  end
 end
