49df08a7971b2d54e76d9a072518d5f4f294c748
Andres Montano <amontano@virginia.edu>
2011-11-15 13:18:53 -0600

    making pop up category helper safe

--- app/helpers/pop_up_categories_helper.rb
+++ app/helpers/pop_up_categories_helper.rb
@@ -3,22 +3,22 @@ module PopUpCategoriesHelper
     selected_category = instance_variable_get("@#{instance_variable_name.to_s}").send(field_name)
     return_str = '<span id="category_name">'
     if selected_category.nil?
-      return_str += '<i>None selected</i>'
+      return_str << '<i>None selected</i>'
       category_selector = pop_up_categories_path
     else
-      return_str += selected_category.title
+      return_str << selected_category.title
       category_selector = pop_up_categories_path(:selected_category_id => selected_category.id)
     end
-    return_str += "</span>\n("
-    return_str += link_to('select parent', category_selector, :class => 'thl-pop no-view-alone no-main-site-ajax', :id => 'category_selector') +
-                  ")\n" +
-                  hidden_field(instance_variable_name, "#{field_name}_id") +
-                  "\n<input type=\"hidden\" id=\"current_category_id\" name=\"current_category_id\" value=\"#{selected_category.id if !selected_category.nil?}\" />\n" +
-                  "\n<input type=\"hidden\" id=\"current_category_title\" name=\"current_category_title\" value=\"#{selected_category.title if !selected_category.nil?}\" />\n" +
-                  "\n<input type=\"hidden\" id=\"current_category_selector\" name=\"current_category_selector\" value=\"#{category_selector}\" />\n" +
-                  "\n<input type=\"hidden\" id=\"selected_category_id\" name=\"selected_category_id\" value=\"#{selected_category.id if !selected_category.nil?}\" />\n" +
-                  "\n<input type=\"hidden\" id=\"selected_category_title\" name=\"selected_category_title\" value=\"#{selected_category.title if !selected_category.nil?}\" />\n" +
-                  "<script type=\"text/javascript\">ActivateThlPopups('#info');</script>"
+    return_str << "</span>\n("
+    return_str << link_to('select parent', category_selector, :class => 'thl-pop no-view-alone no-main-site-ajax', :id => 'category_selector')
+    return_str << ")\n"
+    return_str << hidden_field(instance_variable_name, "#{field_name}_id")
+    return_str << "\n<input type=\"hidden\" id=\"current_category_id\" name=\"current_category_id\" value=\"#{selected_category.id if !selected_category.nil?}\" />\n"
+    return_str << "\n<input type=\"hidden\" id=\"current_category_title\" name=\"current_category_title\" value=\"#{selected_category.title if !selected_category.nil?}\" />\n"
+    return_str << "\n<input type=\"hidden\" id=\"current_category_selector\" name=\"current_category_selector\" value=\"#{category_selector}\" />\n"
+    return_str << "\n<input type=\"hidden\" id=\"selected_category_id\" name=\"selected_category_id\" value=\"#{selected_category.id if !selected_category.nil?}\" />\n"
+    return_str << "\n<input type=\"hidden\" id=\"selected_category_title\" name=\"selected_category_title\" value=\"#{selected_category.title if !selected_category.nil?}\" />\n"
+    return_str << "<script type=\"text/javascript\">ActivateThlPopups('#info');</script>"
     return_str
   end
 end
